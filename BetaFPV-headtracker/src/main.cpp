b6540a0b284bbfc40036b6dc35d92af53ec29359
r-u-t-r-A <98609430+r-u-t-r-A@users.noreply.github.com>
2024-04-17 10:15:27 +0200

    Update main.cpp

--- BetaFPV-headtracker/src/main.cpp
+++ BetaFPV-headtracker/src/main.cpp
@@ -1,6 +1,18 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include "SparkFun_BMI270_Arduino_Library.h"
+
+#define GYRO_CS PA4
+// Create a new sensor object
+BMI270 imu;
+
+void map_data() {
+  
+}
 
 void setup() {
+SerialUSB.begin(115200);
+SerialUSB.println("headtracker booting");
   
 }
 
